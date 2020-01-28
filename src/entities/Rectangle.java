5f5a143a29a6fa319a6dfc2df4f740262247d038
Sn4k3rx <ramonvieira122333@gmail.com>
2020-01-28 02:30:37 -0300

    Implementação da classe retangulo que herdou da abstrata

--- src/entities/Rectangle.java
+++ src/entities/Rectangle.java
@@ -2,5 +2,37 @@ package entities;
 
 public class Rectangle extends Shape{
 
+	private Double widht;
+	private Double height;
+	
+	public Rectangle() {
+		
+	}
+	
+	public Rectangle(Double width, Double height) {
+		this.height = height;
+		this.widht = width;
+	}
+	
+	public Double getWidht() {
+		return widht;
+	}
+
+	public void setWidht(Double widht) {
+		this.widht = widht;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+	@Override
+	public double Area() {
+		return widht * height;
+	}
 
 }
